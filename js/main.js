b86db77748abb6cdd163a73eefb9b1dcc9ebd6b2
AneKstizi <61726575+AneKstizi@users.noreply.github.com>
2020-05-08 22:46:50 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,15 +1,75 @@
 document.addEventListener("DOMContentLoaded", function() {
   const hamburger = document.querySelector('.hamburger-icon');
   const overlay = document.querySelector('.overlay');
+  const menuMobile = document.querySelector('.nav__menu--mobile');
+  const menuMobileItem = document.querySelectorAll('.nav__item--mobile')
+  console.log('menuMobileItem: ', menuMobileItem);
 
   hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
+    menuMobile.classList.toggle('menu--active')
+
+    if (menuMobile.classList.contains('menu--active')) {
+      document.body.style.overflow = 'hidden'
+      TweenMax.staggerFrom('.nav__item--mobile', 1, {
+          opacity: 0,
+          y: 20,
+          ease: Expo.easeInOut
+        }, 0.2
+      )
+      TweenMax.from('.nav__logo__line', 1, {
+          delay: 1,
+          width: 0,
+          ease: Expo.easeInOut
+        }
+      )
+    } else {
+      document.body.style.overflow = 'auto'
+    }
+
   })
 
+  menuMobileItem.forEach(element => {
+    element.addEventListener('click', () => {
+      menuMobile.classList.remove('menu--active')
+    })
+  });
 
   setTimeout(() => {
     overlay.style.opacity = 0;
     overlay.style.visibility = 'hidden';
     document.body.style.overflow = 'auto'
   }, 1000)
+
+
+  // TweenMax
+
+  TweenMax.staggerFrom('.nav__item', 1, {
+      delay: 1,
+      opacity: 0,
+      y: 20,
+      ease: Expo.easeInOut
+    }, 0.2
+  )
+  TweenMax.from('.nav__logo', 1.5, {
+      delay: 1,
+      opacity: 0,
+      y: 20,
+      ease: Expo.easeInOut
+    }
+  )
+  TweenMax.from('.header__descr', 1.5, {
+      delay: 1.1,
+      opacity: 0,
+      x: 100,
+      ease: Expo.easeInOut
+    }
+  )
+  TweenMax.from('.header__bg__wrapper', 1, {
+      delay: 1.3,
+      opacity: 0,
+      y: 100,
+      ease: Expo.easeInOut
+    }
+  )
 });
